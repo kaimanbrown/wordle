1239d97e550fc9c309211b8acd970f0cbcae986e
Kaiman Brown <kaimanbrown@gmail.com>
2015-09-14 20:55:40 +0100

    removed unused code

--- js/wordle.js
+++ js/wordle.js
@@ -14,9 +14,6 @@ var listAttr = function(){
 	  $.each(this.attributes, function() {
 	    // this.attributes is not a plain object, but an array
 	    // of attribute nodes, which contain both the name and value
-	    if(this.specified) {
-	      console.log(this.name, this.value);
-	    }
 	  });
 	});	
 }
